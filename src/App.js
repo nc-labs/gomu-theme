e01dfe73070cf2d619f3e558765dbb8733b594c6
DinhNC <canhdinh2302@gmail.com>
2022-05-27 16:05:32 +0700

    nc-clock page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
+import Indicator from '@components/Indicator'
+import TimeCard from '@components/TimeCard'
 
-const App = () => <>React app</>
+const parseTime = () => {
+  const time = new Date()
+  return {
+    h: time.getHours(),
+    m: time.getMinutes(),
+    s: time.getSeconds(),
+  }
+}
+
+const App = () => {
+  const [time, setTime] = useState(parseTime())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(parseTime())
+    }, 999)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <TimeCard value={time.h} text="hours" />
+      <Indicator />
+      <TimeCard value={time.m} text="min" />
+      <Indicator />
+      <TimeCard value={time.s} text="sec" />
+    </div>
+  )
+}
 
 export default memo(App)
